378cfa0b9286d0db12192415fec8b8f0582de054
Channing Walton <channingwalton@mac.com>
2012-04-13 21:12:04 +0800

    upgrades

--- project/FlyScalaProject.scala
+++ project/FlyScalaProject.scala
@@ -38,7 +38,7 @@ object BuildSettings {
 
 object Dependencies {
   val specs2 = Seq(
-    "org.specs2" %% "specs2" % "1.8.2" % "test->default",
+    "org.specs2" %% "specs2" % "1.9" % "test->default",
     "org.mockito" % "mockito-all" % "1.9.0" % "test->default")
 
   val pegdown = "org.pegdown" % "pegdown" % "1.1.0" % "test"
