ecf96223924575f9ab6d50ceef9885ace0a15bbb
Channing Walton <channingwalton@mac.com>
2013-09-21 21:16:18 +0100

    putting Nige's changes back after a made a mess

--- src/main/scala/com/zink/scala/fly/ScalaFly.scala
+++ src/main/scala/com/zink/scala/fly/ScalaFly.scala
@@ -20,23 +20,41 @@ object ScalaFly {
       case Failure(error) => Left(error)
     }
 }
-
-case class ScalaFly(fly: Fly) {
+trait PrimaryOps {
+  self: ScalaFly =>
 
   def read[T <: AnyRef](template: T, waitTime: Long): Option[T] = Option(fly.read(template, waitTime))
   def write(entry: AnyRef, leaseTime: Long): Long = fly.write(entry, leaseTime)
   def take[T <: AnyRef](template: T, waitTime: Long): Option[T] = Option(fly.take(template, waitTime))
+}
+
+trait MultiOps {
+  self: ScalaFly =>
 
   def writeMany(entries: Iterable[AnyRef], lease: Long): Long = fly.writeMany(entries.toList.asJava, lease)
   def readMany[T <: AnyRef](template: T, matchLimit: Long): Iterable[T] = fly.readMany(template, matchLimit).asScala
   def takeMany[T <: AnyRef](template: T, matchLimit: Long): Iterable[T] = fly.takeMany(template, matchLimit).asScala
+}
+
+trait NotifyOps {
+  self: ScalaFly =>
 
   def notifyWrite(template: AnyRef, handler: Notifiable, leaseTime: Long): Boolean = fly.notifyWrite(template, handler, leaseTime)
   def notifyWrite(template: AnyRef, leaseTime: Long)(block: => Unit): Boolean = fly.notifyWrite(template, new Notifier(block), leaseTime)
   def notifyWrite(template: AnyRef, leaseTime: Long, actor: Actor): Boolean = notifyWrite(template, leaseTime) { actor ! template }
 
-  def snapshot(template: AnyRef): AnyRef = fly.snapshot(template)
+  def notifyTake(template: AnyRef, handler: Notifiable, leaseTime: Long): Boolean = fly.notifyTake(template, handler, leaseTime)
+  def notifyTake(template: AnyRef, leaseTime: Long)(block: => Unit): Boolean = fly.notifyTake(template, new Notifier(block), leaseTime)
+  def notifyTake(template: AnyRef, leaseTime: Long, actor: Actor): Boolean = notifyTake(template, leaseTime) { actor ! template }
+}
 
+trait PerformanceOps {
+  self: ScalaFly =>
+
+  def snapshot(template: AnyRef): AnyRef = fly.snapshot(template)
+}
+
+case class ScalaFly(fly: Fly) extends PrimaryOps with MultiOps with NotifyOps with PerformanceOps {
   class Notifier(f: => Unit) extends NotifyHandler {
     def templateMatched() = f
   }
