23870277c011c41b5faab26e6d1dbb144f3ac1d6
Channing Walton <channingwalton@mac.com>
2014-06-18 20:56:53 +0100

    what a mess

--- project/FlyScalaProject.scala
+++ project/FlyScalaProject.scala
@@ -9,8 +9,8 @@ object BuildSettings {
 
   val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq[Setting[_]](
     organization := "com.flyobjectspace",
-    version := "2.1.4",
-    scalaVersion := "2.10.5",
+    version := "2.1.5",
+    scalaVersion := "2.10.4",
     scalacOptions += "-target:jvm-1.6",
 
     packageDist <<= (baseDirectory, crossTarget, version, packageBin in Compile, packageDoc in Compile, packageSrc in Compile, streams) map {
